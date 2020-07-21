bec7fa1312b3fe984604fa54b4211b66062194d1
Morgan Hayes <morgan@ccenttcs.com>
2020-07-21 13:12:07 -0600

    bt2

--- app.js
+++ app.js
@@ -337,29 +337,29 @@ function readBatteryLevel() {
     .catch(error => { console.error(error);
   });
 
-  navigator.bluetooth.requestDevice({
-      filters: [{
-        services: ['battery_service']
-      }]
-    })
-    .then(function (device) {
-      return device.gatt.connect();
-    })
-    .then(function (server) {
-      return server.getPrimaryService('battery_service');
-    })
-    .then(function (service) {
-      return service.getCharacteristic('battery_level');
-    })
-    .then(function (characteristic) {
-      return characteristic.readValue();
-    })
-    .then(function (value) {
-      $target.innerHTML = 'Battery percentage is ' + value.getUint8(0) + '.';
-    })
-    .catch(function (error) {
-      $target.innerText = 'error found in catch -- ' + error;
-    });
+  // navigator.bluetooth.requestDevice({
+  //     filters: [{
+  //       services: ['battery_service']
+  //     }]
+  //   })
+  //   .then(function (device) {
+  //     return device.gatt.connect();
+  //   })
+  //   .then(function (server) {
+  //     return server.getPrimaryService('battery_service');
+  //   })
+  //   .then(function (service) {
+  //     return service.getCharacteristic('battery_level');
+  //   })
+  //   .then(function (characteristic) {
+  //     return characteristic.readValue();
+  //   })
+  //   .then(function (value) {
+  //     $target.innerHTML = 'Battery percentage is ' + value.getUint8(0) + '.';
+  //   })
+  //   .catch(function (error) {
+  //     $target.innerText = 'error found in catch -- ' + error;
+  //   });
   };
 
 
